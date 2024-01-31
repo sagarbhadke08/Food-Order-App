7909c2b1f1d28a67be419c052e33374f667f4525
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-01-31 20:41:17 +0530

    updated

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-
+import MealItem from "./MealItem.jsx";
 export default function Meals() {
 
     const [loadedMeals, setLoadedMeals] = useState([]);
@@ -19,9 +19,14 @@ export default function Meals() {
     }, []);
 
 
-    return <ul id="meals">
-        {loadedMeals.map((meal) =>( <li key={meal.id} >{meal.name}</li>))}
-    </ul>;
-
+    return (
+        <ul id="meals">
+            {loadedMeals.map((meal) =>
+            (
+                <MealItem key={meal.id} meal={meal} />
+            ))
+            }
+        </ul>
+    );
 
 }
