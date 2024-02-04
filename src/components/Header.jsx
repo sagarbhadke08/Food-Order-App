22a9373529043e4e25ae151dbc8a9313e59fa4fe
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-04 23:12:02 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import logoImg from '../assets/logo.jpg'
-
+import Button from './UI/Button.jsx'
 export default function Header() {
 
     return (
@@ -10,7 +10,7 @@ export default function Header() {
                     <h1>ReactFood</h1>
                 </div>
                 <nav>
-                    <button>Cart (0)</button>
+                    <Button textOnly={true}>Cart (0)</Button>
                 </nav>
             </header>
          
