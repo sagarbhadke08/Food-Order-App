2c25ff94c89c1d0bbf81c4cde61aa5a42ebe95d8
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-05 09:39:45 +0530

    updated

--- src/Store/CartContext.jsx
+++ src/Store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext , useReducer} from "react";
+import { createContext, useReducer } from "react";
 
 const CartContext = createContext({
     items: [],
@@ -6,22 +6,39 @@ const CartContext = createContext({
     removeItem: (id) => { },
 });
 
-function cartReducer(state, action){
-    if(action.type==='ADD-ITEM'){
+function cartReducer(state, action) {
+    if (action.type === 'ADD-ITEM') {
         //update the state item to a meal item
+        const existingCartIndex = state.item.findIndex(
+            (item) => state.id === action.item.id
+        );
+
+        const updatedItems = [...state.items];
+
+        if (existingCartIndex > -1) {
+
+
+
+            const updatedItem = {
+                ...state.items[existingCartIndex],
+                quantity: state.items[existingCartIndex].quantity + 1;
+            }
+        } else {
+            updatedItems.push(action.item)
+        }
     }
 
-    if(action.type==='REMOVE-ITEM'){
+    if (action.type === 'REMOVE-ITEM') {
         //remove the state item from    
     }
 
     return state;
-    
+
 }
 
 export function CartContextProvider({ children }) {
 
-    useReducer();
+    useReducer(cartReducer, { items: [] });
     return <CartContext.Provider>{children}</CartContext.Provider>
 }
 
