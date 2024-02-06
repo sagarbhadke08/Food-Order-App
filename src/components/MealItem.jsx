44016c0cbe418f278c6f73ec19cdcc93e72bb6a8
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-06 06:37:10 +0530

    up

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,17 +1,27 @@
 import Button from "./UI/Button.jsx"
 import { CurrencyFormatter } from "../util/Formatting.js"
+import { useContext } from "react"
+import CartContext from "../Store/CartContext.jsx"
 
-export default function MealItem({meal}){
+export default function MealItem({ meal }) {
+
+    const cartCtx = useContext(CartContext);
+
+    function handleAddMealToCart() {
+        cartCtx.addItem(meal)
+    }
+
+
     return <li className="meal-item">
         <article>
-            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name}/>
+            <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
             <div>
                 <h3>{meal.name}</h3>
                 <p className="meal-item-price">{CurrencyFormatter.format(meal.price)}</p>
                 <p className="meal-item-description">{meal.description}</p>
             </div>
             <p className="meal-item-actions">
-                <Button>Add to Cart</Button>
+                <Button onClick={handleAddMealToCart}>Add to Cart</Button>
             </p>
         </article>
     </li>
