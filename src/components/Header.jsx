1ee0d8bbbe85cd39d7a9e2d63d0472dee3af9473
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-12 06:41:46 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,24 @@ import { useContext } from 'react';
 import logoImg from '../assets/logo.jpg';
 import Button from './UI/Button.jsx';
 import CartContext from '../Store/CartContext.jsx';
-
+import UserProgressContext from '../Store/UserProgressContext.jsx';
 
 export default function Header() {
 
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(UserProgressContext);
 
     const totalCartItems = cartCtx.items.reduce((totalNumberOfItems, item) => {
 
         totalNumberOfItems + item.quantity;
     }, 0);
+
     console.log(cartCtx);
     // console.log(totalNumberOfItems);
+
+    function handleShowCart() {
+        userProgressCtx.showCart();
+    }
 
     return (
         <>
@@ -24,7 +30,7 @@ export default function Header() {
                     <h1>ReactFood</h1>
                 </div>
                 <nav>
-                    <Button textOnly >Cart ({totalCartItems})</Button>
+                    <Button textOnly onClick={handleShowCart}>Cart ({totalCartItems})</Button>
                 </nav>
             </header>
 
