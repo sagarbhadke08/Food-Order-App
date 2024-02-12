3a0aa67a557323d96a8c05875aea4d476b1e9111
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-12 06:41:56 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import Header from "./components/Header.jsx";
 import Meals from "./components/Meals.jsx";
-import {CartContextProvider} from './Store/CartContext.jsx';
-
+import Cart from "./components/Cart.jsx";
+import { CartContextProvider } from './Store/CartContext.jsx';
+import { UserProgressContextProvider } from "./Store/UserProgressContext.jsx";
 function App() {
   return (
-    <CartContextProvider>
-      <Header />
-      <Meals />
-    </CartContextProvider>
+    <UserProgressContextProvider>
+      <CartContextProvider>
+        <Header />
+        <Meals />
+        <Cart/>
+      </CartContextProvider>
+    </UserProgressContextProvider>
   );
 }
 
