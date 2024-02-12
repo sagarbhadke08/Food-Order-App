29ad7461e805b7572de1392035ead5c084045d21
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-02-12 06:41:35 +0530

    updated

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,17 +2,20 @@ import { useContext } from "react";
 
 import Modal from "./UI/Modal.jsx";
 import CartContext from "../Store/CartContext.jsx";
+import userProgressContext from "../Store/UserProgressContext.jsx"
 import { CurrencyFormatter } from "../util/Formatting.js";
 import Button from "./UI/Button.jsx";
 export default function Cart() {
 
     const cartCtx = useContext(CartContext);
+    const userProgressCtx = useContext(userProgressContext);
+
 
     const cartTotal = cartCtx.items.reduce(
-        (totalPrice, item) => totalPrice + item.quantity * item.price,0
+        (totalPrice, item) => totalPrice + item.quantity * item.price, 0
     )
 
-    return <Modal className="cart">
+    return <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
         <h2>Your Cart</h2>
         <ul>
             {cartCtx.items.map((item) => (
