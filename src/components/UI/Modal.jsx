41c7854044e914382945a759c84e6471a82f39cf
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-03-24 23:36:44 +0530

    updated

--- src/components/UI/Modal.jsx
+++ src/components/UI/Modal.jsx
@@ -2,14 +2,14 @@ import { useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
 export default function Modal({ children, open, className = '' }) {
-
+    const dialog = useRef();
     useEffect(() => {
         if (open) {
             dialog.current.showModal();
         }
     }, [open]);
 
-    const dialog = useRef();
+
     return createPortal(
         <dialog ref={dialog} className={`modal ${className}`}>{children}</dialog>,
 
