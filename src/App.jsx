28e0c2836cb4a4af964301916fa33a858b883eb8
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-03-25 13:11:18 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
       <CartContextProvider>
         <Header />
         <Meals />
-        <Cart/>
+        <Cart />
       </CartContextProvider>
     </UserProgressContextProvider>
   );
