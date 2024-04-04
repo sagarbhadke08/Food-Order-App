8fec562d6d2fe5723aff4239316b7246cb0aa8f0
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-04-05 00:03:59 +0530

    updated

--- src/Store/CartContext.jsx
+++ src/Store/CartContext.jsx
@@ -80,7 +80,7 @@ export function CartContextProvider({ children }) {
         removeItem,
     }
 
-   // console.log(cartContext);
+    // console.log(cartContext);
 
     return <CartContext.Provider value={cartContext}>{children}</CartContext.Provider>
 }
