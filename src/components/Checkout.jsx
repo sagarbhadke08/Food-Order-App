ef5a60dd2bd5432769d75bc7862496b8a09fa473
sagarbhadke08 <sagarbhadke08@gmail.com>
2024-04-08 00:24:18 +0530

    updated

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,26 +1,46 @@
 import { useContext } from "react";
 import Modal from "./UI/Modal.jsx";
 import CartContext from "../Store/CartContext.jsx";
-import { CurrencyFormatter } from "../util/Formatting.jsx";
-CurrencyFormatter
+import { CurrencyFormatter } from "../util/Formatting.js";
+import Input from "./Input.jsx";
+import UserProgressContext from "../Store/UserProgressContext.jsx";
+import Button from "./UI/Button.jsx";
+
 
 
 
 export default function Checkout() {
     const cartCtx = useContext(CartContext)
+    const userProgressCtx = useContext(UserProgressContext);
 
     const cartTotal = cartCtx.items.reduce(
         (totalPrice, item) => totalPrice + item.quantity * item.price,
         0
     );
 
+function handleClose(){
+    userProgressCtx.hideCheckout();
 
-    return <Modal>
-        <form action="">
-            <h2>
-                Checkout
-            </h2>
-            <p>Total Amount: {CurrencyFormatter.format(cartTotal)}</p>
-        </form>
-    </Modal>
+}
+
+    return (
+        <Modal open={userProgressCtx.progress === 'checkout'}>
+            <form action="">
+                <h2>Checkout</h2>
+                <p>Total Amount: {CurrencyFormatter.format(cartTotal)}</p>
+
+                <Input label="Full Name" type="text" id="full-name" />
+                <Input label="E-Mail Address" type="email" id="email" />
+                <Input label="Street" type="text" id="street" />
+                <div>
+                    <Input label="Postal Code" type="text" id="postal code" />
+                    <Input label="City" type="text" id="city" />
+                </div>
+                <p className="modal-actions">
+                    <Button type="button" textOnly onClick={handleClose} >Close</Button>
+                    <Button>Submit Order</Button>
+                </p>
+            </form>
+        </Modal>
+    );
 }
